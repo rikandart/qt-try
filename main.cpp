7d96b2f6ae73dacc3edf50f4418852cf769fb786
rikandart <pizhun97@gmail.com>
2020-02-29 19:11:32 +0300

    evry ting ok

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 	// comment one
+<<<<<<< HEAD
+=======
+	// comment added
+>>>>>>> another-branch
     w.show();
     return a.exec();
 }
