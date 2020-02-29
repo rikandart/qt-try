e88b8e38645479136521f4e7731f1118a02c25f5
rikandart <pizhun97@gmail.com>
2020-02-29 19:21:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 	// comment one
-<<<<<<< HEAD
-=======
-	// comment added
->>>>>>> another-branch
     w.show();
     return a.exec();
 }
