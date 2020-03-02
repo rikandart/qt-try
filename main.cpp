3705c5771559647f35c0bbd99125bb0e08762b62
rikandart <pizhun97@gmail.com>
2020-03-02 19:40:59 +0300

    test inheritance

--- main.cpp
+++ main.cpp
@@ -2,6 +2,38 @@
 
 #include <QApplication>
 
+//#define _TEST_INHERITANCE
+
+#ifdef _TEST_INHERITANCE
+class first{
+public:
+    int height = 0;
+    first();
+    first(int i);
+    ~first();
+
+};
+
+first::first(){
+
+}
+
+first::first(int i){
+    height = i;
+}
+
+class second : public first{
+public:
+    second(int y);
+    first *h;
+    int k(int p);
+};
+
+second::second(int y) : first(y), h(new first(y)){
+
+}
+#endif
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
