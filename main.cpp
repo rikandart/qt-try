252fe22b22edd882a28a4cd6e5c852e94c4eff3d
rikandart <pizhun97@gmail.com>
2020-03-02 20:06:34 +0300

    20-06 020320

--- main.cpp
+++ main.cpp
@@ -38,7 +38,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-	// comment from another-branch
     w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked(){
+    MainWindow::ui->pushButton->hide();
+}
